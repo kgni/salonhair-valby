0a727ff5fd4b48055a95ec5afcbc1dec10c2c994
mdiadk <acc@mdia.dk>
2022-04-10 21:39:33 +0200

    add loader, fix map, padding margin

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const loader = document.querySelector('.loader-container');
+const main = document.querySelector('main');
+
+function init() {
+	setTimeout(() => {
+		loader.style.opacity = 0;
+		loader.style.display = 'none';
+		main.style.display = 'block';
+		setTimeout(() => (main.style.opacity = 1), 50);
+	}, 4000);
+}
+
+init();
