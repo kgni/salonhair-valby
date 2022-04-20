ed5a30c22a3e02a9071bb951bae7e2ff36611299
kgni <admin@mdia.dk>
2022-04-20 12:07:45 +0200

    add about img, add AOS functionality

--- js/main.js
+++ js/main.js
@@ -6,8 +6,8 @@ function init() {
 		loader.style.opacity = 0;
 		loader.style.display = 'none';
 		main.style.display = 'block';
-	// 	setTimeout(() => (main.style.opacity = 1), 50);
-	// }, 3000);
+		setTimeout(() => (main.style.opacity = 1), 50);
+	}, 0);
 }
 
 init();
