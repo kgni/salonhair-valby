311791a86ff1c76fe3c6581626d28bb110a94679
kgni <admin@mdia.dk>
2022-04-28 14:31:58 +0200

    add manual functionality for slider

--- js/main.js
+++ js/main.js
@@ -11,3 +11,27 @@ function init() {
 }
 
 init();
+
+// Slider
+
+const btn1 = document.querySelector('.manual-btn1');
+const btn2 = document.querySelector('.manual-btn2');
+const btn3 = document.querySelector('.manual-btn3');
+
+btn1.addEventListener('click', () => {
+	btn1.classList.add('slide-active');
+	btn2.classList.remove('slide-active');
+	btn3.classList.remove('slide-active');
+});
+
+btn2.addEventListener('click', () => {
+	btn2.classList.add('slide-active');
+	btn1.classList.remove('slide-active');
+	btn3.classList.remove('slide-active');
+});
+
+btn3.addEventListener('click', () => {
+	btn3.classList.add('slide-active');
+	btn1.classList.remove('slide-active');
+	btn2.classList.remove('slide-active');
+});
